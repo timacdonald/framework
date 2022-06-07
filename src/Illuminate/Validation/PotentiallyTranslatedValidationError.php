9843b055f9f0569ef5a1b9e5846a12cf94c662ff
Tim MacDonald <hello@timacdonald.me>
2022-06-07 11:57:24 +1000

    wip

---
+++ src/Illuminate/Validation/PotentiallyTranslatedValidationError.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Illuminate\Validation;
+
+use Illuminate\Translation\PotentiallyTranslatedString;
+
+class PotentiallyTranslatedValidationError extends PotentiallyTranslatedString
+{
+    public function __construct()
+    {
+
+    }
+}
