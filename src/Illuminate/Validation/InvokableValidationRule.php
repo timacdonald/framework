da4a4e60aab3136cc88f194b7b419f0442facd8c
Tim MacDonald <hello@timacdonald.me>
2022-06-07 12:23:21 +1000

    wip

--- src/Illuminate/Validation/InvokableValidationRule.php
+++ src/Illuminate/Validation/InvokableValidationRule.php
@@ -132,8 +132,20 @@ class InvokableValidationRule implements RuleContract, DataAwareRule, ValidatorA
             $this->validator->getTranslator(),
             fn ($message) => $this->messages[] = $message
         ) extends PotentiallyTranslatedString {
+            /**
+             * The callback to call when the object destructs.
+             *
+             * @var \Closure
+             */
             protected $destructor;
 
+            /**
+             * Create a new pending potentially translated string.
+             *
+             * @param  string  $string
+             * @param  \Illuminate\Contracts\Translation\Translator  $translator
+             * @param  \Closure  $destructor
+             */
             public function __construct($message, $translator, $destructor)
             {
                 parent::__construct($message, $translator);
