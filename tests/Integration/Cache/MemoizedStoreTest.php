78df084bcdcd6a5368f484be9b63888c2ae4759e
Tim MacDonald <hello@timacdonald.me>
2025-01-16 10:20:07 +1100

    wip

--- tests/Integration/Cache/MemoizedStoreTest.php
+++ tests/Integration/Cache/MemoizedStoreTest.php
@@ -282,6 +282,7 @@ class MemoizedStoreTest extends TestCase
 
     public function test_memoized_keys_are_prefixed()
     {
+        // HERE
         $redis = Cache::store('redis');
 
         $redis->setPrefix('aaaa');
