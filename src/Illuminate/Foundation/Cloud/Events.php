3a2bbb1f12d392ed10939e3558b41e2eb383acb2
Tim MacDonald <hello@timacdonald.me>
2026-04-22 15:28:29 +1000

    add event implementation

--- src/Illuminate/Foundation/Cloud/Events.php
+++ src/Illuminate/Foundation/Cloud/Events.php
@@ -2,15 +2,153 @@
 
 namespace Illuminate\Foundation\Cloud;
 
+use Symfony\Component\Translation\Exception\RuntimeException;
+use Throwable;
+
 class Events
 {
+    protected const int CONNECTION_TIMEOUT = 10;
+    protected const int TIMEOUT = 2;
+
+    /**
+     * @var resource|null
+     */
+    protected $socket = null;
+
+    /**
+     * @param  array<string, mixed>  $payload
+     */
     public function emit(array $payload): void
     {
         $this->emitMany([$payload]);
     }
 
+    /**
+     * @param  list<array<string, mixed>>  $payloads
+     */
     public function emitMany(array $payloads): void
     {
-        //
+        try {
+            $this->write($this->format($payloads));
+        } catch (Throwable $e) {
+            // TODO $e
+        }
+    }
+
+    protected function write(string $payload): void
+    {
+        $originalPayloadLength = strlen($payload);
+        $written = 0;
+
+        while (true) {
+            $thisWrite = fwrite($this->socket, $payload);
+
+            if ($thisWrite === false) {
+                $message = $this->withSocketMetaData('Unable to write to socket');
+
+                $this->disconnect();
+
+                throw new RuntimeException($message);
+            }
+
+            $written += $thisWrite;
+
+            if ($written >= $originalPayloadLength) {
+                return;
+            }
+
+            $payload = substr($payload, $thisWrite);
+        }
+    }
+
+    /**
+     * @param  list<array<string, mixed>>  $payloads
+     */
+    protected function format(array $payloads)
+    {
+        return array_reduce($payloads, function (string $carry, array $line) {
+                if ($carry !== '') {
+                    $carry .= "\n";
+                }
+
+                return $carry .= json_encode($line, flags: JSON_THROW_ON_ERROR | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRESERVE_ZERO_FRACTION | JSON_INVALID_UTF8_SUBSTITUTE);
+            }, '');
+    }
+
+    protected function ensureConnected(): void
+    {
+        if (! $this->connected()) {
+            $this->connect();
+        }
+    }
+
+    protected function connect(): void
+    {
+        $socket = stream_socket_client(
+            address: $_ENV['LARAVEL_CLOUD_LOG_SOCKET'] ?? $_SERVER['LARAVEL_CLOUD_LOG_SOCKET'] ?? 'unix:///tmp/cloud-init.sock',
+            error_code: $errorCode,
+            error_message: $errorMessage,
+            timeout: self::CONNECTION_TIMEOUT,
+            // TODO flags: STREAM_CLIENT_CONNECT | STREAM_CLIENT_PERSISTENT
+        );
+
+        if ($socket === false) {
+            throw new RuntimeException("Failed connecting to the socket: {$errorMessage} [{$errorCode}]");
+        }
+
+        if (! stream_set_timeout($socket, self::TIMEOUT)) {
+            $message = $this->withSocketMetaData("Failed configuring socket timeout");
+
+            $this->disconnect();
+
+            throw new RuntimeException($message);
+        }
+
+        $this->socket = $socket;
+    }
+
+    protected function connected(): bool
+    {
+        if (gettype($this->socket) !== 'resource') {
+            return false;
+        }
+
+        $meta = stream_get_meta_data($this->socket);
+
+        return ! $meta['timed_out'] && ! $meta['eof'];
+    }
+
+    protected function disconnect(): void
+    {
+        try {
+            fclose($this->socket);
+        } catch (Throwable) {
+            //
+        }
+
+        $this->socket = null;
+    }
+
+    protected function withSocketMetaData(string $message): string
+    {
+        $prefix = "{$message}\n---\n";
+
+        if ($this->connected()) {
+            return "{$prefix}closed: true";
+        }
+
+        $meta = stream_get_meta_data($this->socket);
+
+        return $prefix.array_reduce(array_keys($meta), function ($carry, $key) use ($meta) {
+            try {
+                return $carry.$key.': '.match ($meta[$key]) {
+                    true => 'true',
+                    false => 'false',
+                    default => $meta[$key],
+                }.PHP_EOL;
+            } catch (Throwable) {
+                return $carry;
+            }
+        }, '');
     }
 }
