e0a457413af13e82b6afd873860b4e852caab3db
Tim MacDonald <hello@timacdonald.me>
2026-04-27 16:41:56 +1000

    wip

--- src/Illuminate/Foundation/Cloud.php
+++ src/Illuminate/Foundation/Cloud.php
@@ -33,8 +33,6 @@ class Cloud
      */
     public static function bootstrapperBootstrapped(Application $app, string $bootstrapper): void
     {
-        // TODO bind events as singleton
-
         (match ($bootstrapper) {
             LoadConfiguration::class => function () use ($app) {
                 static::configureDisks($app);
