094c44f44eb81deec57cc95533f6cc5c5ada8c76
Tim MacDonald <hello@timacdonald.me>
2026-04-28 15:33:11 +1000

    wip

--- src/Illuminate/Foundation/Cloud/QueueConnector.php
+++ src/Illuminate/Foundation/Cloud/QueueConnector.php
@@ -41,10 +41,10 @@ class QueueConnector implements ConnectorInterface
     {
         Worker::$restartable = false;
 
-        $this->app['events']->listen(fn (WorkerStopping $event) => match ($event->reason) {
-            WorkerStopReason::TimedOut => $this->app['queue.failer']->onJobTimeout(),
-            default => $queue->onWorkerStopping(),
-        });
+        /* $this->app['events']->listen(fn (WorkerStopping $event) => match ($event->reason) { */
+        /*     WorkerStopReason::TimedOut => $this->app['queue.failer']->onJobTimeout(), */
+        /*     default => $queue->onWorkerStopping(), */
+        /* }); */
     }
 
     /**
